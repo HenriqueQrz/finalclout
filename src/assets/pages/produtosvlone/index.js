4f07b6c516f7538c6099ae9c15dbc62b61e3fef0
HenriqueQrz <riqueto.queiroz@gmail.com>
2023-11-24 09:31:27 -0300

    md

--- src/assets/pages/produtosvlone/index.js
+++ src/assets/pages/produtosvlone/index.js
@@ -80,10 +80,19 @@ function Consultavlone()
     navigate('/')
   }
 
+  function supreme(){
+    navigate ('/supreme')
+  }
+
+  function assc(){
+    navigate ('/assc')
+  }
+
+
 
   return (
 <div className='principal'>
-    <div className="cabecalhoo">
+    <div className="cabecalho">
     <div className="imgesquerda">
       <img src={logo1} onClick={logo}/>
       <img src={logo2} onClick={logo} />
@@ -96,7 +105,7 @@ function Consultavlone()
     <img className="lupa" src={logo3} />
     <img className="lupa1" src={logo5} />
 
-    <h2 className="tex2"> Minha conta <br></br> Conecte-se </h2>
+    <h2 className="tex2"> Conecte-se </h2>
 
     <img className="lupa" src={logo4} />
     <h2 className="tex2"> Carrinho <br></br> R$ 0,00 BRL </h2>
@@ -115,13 +124,13 @@ function Consultavlone()
 
   <div className='logosmarcaacesso'>
 
-<img src={highlogo} onClick={high} />
-<img src={sufganglogo} onClick={sufgang}/> 
-<img src={vlonelogo} onClick={vlone}/>
-<img src={palacelogo} onClick={palace}/>
-<img src={supremelogo} />
-<img src={assclogo} />
-<img src={offwhite} /> 
+  <img src={highlogo} onClick={high} />
+        <img src={sufganglogo} onClick={sufgang} /> 
+        <img src={vlonelogo} onClick={vlone} />
+        <img src={palacelogo} onClick={palace}/>
+        <img src={supremelogo} onClick={supreme} />
+        <img src={assclogo} onClick={assc} />
+        <img src={offwhite} /> 
 
 </div>
 
